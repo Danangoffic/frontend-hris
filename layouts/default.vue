c1c956884321e377ac7251359c7934125475d313
Danang Arif Rahmanda <darifrahmanda@gmail.com>
2023-07-16 04:02:55 +0700

    init

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  async asyncData({ params, $http }) {
+    const post = await $http.$get(
+      `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    );
+    return { post };
+  },
+  data() {
+    return {
+      post: {},
+    };
+  },
+};
+</script>
